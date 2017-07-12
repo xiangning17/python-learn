772d0af1aa0d9df2f57c5464abc9712e4a161bb3
ning.xiang <ning.xiang@tcl.com>
2017-07-12 17:06:55 +0800

    简化后的cts_sync,使用redis存取数据，AsyncExecutor多线程执行爬取操作

--- cts_sync.py
+++ cts_sync.py
@@ -1,87 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import pprint
+import logging
+import os
+import redis
 import urllib
 from collections import namedtuple
 from functools import partial
-from threading import Lock
 from time import *
 from urlparse import urlparse
 
-from bisect import insort
-from jinja2 import Environment, FileSystemLoader, Template
-
-import logging
-
+from AsyncExecutor import async
 from bs4 import BeautifulSoup
 
-import os
-
-# from tomorrow import threads 另一种多线程执行
-
 HOST = "http://172.26.35.223"
-LOCAL_DIR = "/data/cephfs2"
-LOCAL_LOG_DIR = os.path.join(LOCAL_DIR, "cts_log")
+LOCAL_DIR = "/data/ubuntu16/ctsdemo"
 
 FileStruct = namedtuple("FileStruct", ("is_dir", "url", "name", "modify_time"))
-FileStruct.__lt__ = lambda x, y: x.name.upper() < y.name.upper()  # 为支持bisect排序
 
 LOG_FORMAT = '%(asctime)s - %(filename)s[line:%(lineno)d] - %(threadName)s - %(levelname)s: %(message)s'
-logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
-file_handler = None
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+
+r = redis.Redis(host='127.0.0.1', port=6379)
+
+REDIS_KEY_DIRS = 'cts_dirs'
+REDIS_KEY_FILES = 'cts_files'
+REDIS_KEY_UPDATES = 'cts_updates'
 
 
-def get_file_logging_handler(filename):
-    fh = logging.FileHandler(os.path.join(LOCAL_LOG_DIR, filename))
-    fh.setFormatter(logging.Formatter(LOG_FORMAT))
-    return fh
-
-
-def cts_sync():
-    global file_handler
-    logger = logging.getLogger()
-    logger.removeHandler(file_handler)
-    file_handler = get_file_logging_handler(strftime('%Y-%m-%d %H:%M:%S') + ".log")
-    logger.addHandler(file_handler)
-
-    sync_dir(HOST + "/cts/cts_document/")
-
-    file_handler.flush()
-
-
-from MultiThreadExecutor2 import MultiThreadExecutor
-
-mte = MultiThreadExecutor(30)
-
-
-@mte.async
+@async(15)
 def sync_dir(url):
     ps = urlparse(url)
 
     path_local = unicode(urllib.unquote(str(ps.path)), "utf-8")  # 从url转化为路径形式，具体为“urlencode -> utf-8 -> unicode”
     path_local = LOCAL_DIR + path_local
-    logging.debug("sync dir %s, path = %s" % (url, path_local))
+    logging.info("sync dir %s, path = %s" % (url, path_local))
 
     if not os.path.exists(path_local):  # 若本地不存在该目录，则创建
         os.makedirs(path_local)
 
-    html = urllib.urlopen(url)
+    try:
+        html = urllib.urlopen(url)
+    except Exception as e:
+        # 访问量过大时出现IOERROR， “IOError: [Errno socket error] [Errno 104] Connection reset by peer”
+        logging.exception(e)
+        sync_dir(url)
+        return
+
+    r.sadd(REDIS_KEY_DIRS, path_local)  # 记录文件夹到数据库
+
     bs = BeautifulSoup(html, "html.parser")
     rows = bs.select("table tr")
 
     parse_row = partial(_parse_row, url=url, path_local=path_local)
+
     files = map(parse_row, rows[3:-1])  # 解析Tag， 返回FileStruct列表
 
     require_update = filter(update, files)  # 得到需要更新的文件（文件夹总是在返回结果中，因为还需要判断其中的文件是否有更新）
-    logging.debug("%s require_update %d:\n%s " % (path_local, len(require_update), pprint.pformat(require_update)))
+    logging.info("%s require_update %d:\n%s " % (path_local, len(require_update), pprint.pformat(require_update)))
 
     for f in require_update:
         if f.is_dir:
             sync_dir(f.url)
         else:
             download(f)
-
-    return FileStruct(True, "finish sync %s" % url, "name", "time")
 
 
 def _parse_row(tag, url, path_local):
@@ -102,74 +83,45 @@ def _parse_row(tag, url, path_local):
     modify_time = next(children).text.strip()  # 去除字符串前后空格
     modify_time = int(mktime(strptime(modify_time, "%d-%b-%Y %H:%M")))  # 转换时间字符串为time整数, 原格式为“12-May-2016 18:45”
 
-    # logging.debug("FileStruct : " + str((is_dir, url, name, modify_time),))
     return FileStruct(is_dir, url, name, modify_time)
 
 
 def update(f):
     """根据给出的FileStruct判断是否需要更新本地的文件（夹），
     当文件（夹）不存在或者当前FileStruct代表的是文件夹亦或是文件的修改时间与FileStruct中修改时间不一致时返回True，即为需要修改"""
-    return not os.path.exists(f.name) or os.path.isdir(f.name) or os.path.getmtime(f.name) != f.modify_time
+    if f.is_dir:
+        return True
+
+    changed = r.hget(REDIS_KEY_FILES, f.name) != str(f.modify_time)     # redis中取出的值总是字符串
+    if changed:
+        r.hset(REDIS_KEY_FILES, f.name, f.modify_time)
+    return changed
 
 
-@mte.async
+@async(15)
 def download(file_struct):
-    urllib.urlretrieve(file_struct.url, file_struct.name)  # 下载文件
-    os.utime(file_struct.name, (file_struct.modify_time, file_struct.modify_time))  # 设置文件的修改时间为网站上获取到的时间
-    logging.debug("finished download %s from %s" % (file_struct.name, file_struct.url))
+    # try:
+    #     urllib.urlretrieve(file_struct.url, file_struct.name)  # 下载文件
+    # except Exception as e:
+    #     # 访问量过大时出现IOERROR， “IOError: [Errno socket error] [Errno 104] Connection reset by peer”
+    #     logging.exception(e)
+    #     download(file_struct)
+    #     return
 
-    record(file_struct)
-
-
-update_files = []
-record_lock = Lock()
-
-
-def record(file_struct):
-    with record_lock:
-        insort(update_files, file_struct)
-
-
-def display_file(files):
-    for f in files:
-        yield FileStruct(f.is_dir, os.path.dirname(f.url), f.name[len(LOCAL_DIR):],
-                         strftime("%d-%b-%Y %H:%M", gmtime(f.modify_time)))
-
-
-def dump_record(name):
-    template = """<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html
-        PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-    <title>Updates Cts Files</title>
-</head>
-<body>
-
-<h1>Updates Cts Files</h1>
-
-<table>
-    <tr><th>File</th><th>Modify time</th></tr>
-    {% for file in files %}
-    <tr><td><a href='{{file.url}}'>{{file.name}}</a></td><td>{{file.modify_time}}</td></tr>
-    {% endfor %}
-</table>
-</body>
-</html>"""
-    tp = Template(template)
-    fn = os.path.join(LOCAL_LOG_DIR, 'updates-%s.html' % name)
-    with open(fn, 'a+') as f:
-        f.write(tp.render(files=display_file(update_files)).encode('utf-8'))
+    logging.info("finished download %s from %s" % (file_struct.name, file_struct.url))
+    # 去除本地路径前缀，保存更新过的文件到redis数据库
+    r.sadd(REDIS_KEY_UPDATES, file_struct.name[len(LOCAL_DIR):])
 
 
 if __name__ == '__main__':
     start = time()
 
-    cts_sync()
-    mte.join()
+    sync_dir(HOST + "/cts/")
+    sync_dir.join()
+    download.join()
 
-    dump_record(strftime('%Y-%m-%d %H:%M:%S'))
-
+    logging.info('dir count are %s' % r.scard(REDIS_KEY_DIRS))
+    logging.info('file count are %s' % r.hlen(REDIS_KEY_FILES))
+    logging.info('update count are %s' % r.scard(REDIS_KEY_UPDATES))
     end = time()
-    logging.debug("finish sync cts from server! Spent time : %f " % (end - start))
+    logging.info("finish sync cts from server! Spent time : %f seconds" % (end - start))
