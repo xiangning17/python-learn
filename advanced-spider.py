f706be9527a8530e40fc3335a86fd21f90f032f6
ning.xiang <ning.xiang@tcl.com>
2017-07-12 16:34:27 +0800

    使用多线程执行器执行获取url的方法

--- advanced-spider.py
+++ advanced-spider.py
@@ -1,23 +1,35 @@
 # coding: utf-8
 import redis
-import urlparse
+from urlparse import urlparse, urlunparse, urljoin
+from time import time
 
 import requests
-
 from bs4 import BeautifulSoup
 
+import logging
 
-# r = redis.Redis(host='127.0.0.1', port=6379)
+from AsyncExecutor import async
+
+# 连接redis服务器
+r = redis.Redis(host='127.0.0.1', port=6379)
+r.flushall()
 
 HOST = "http://172.26.35.223"
 
 
+LOG_FORMAT = '%(asctime)s - %(filename)s[line:%(lineno)d] - %(threadName)s - %(levelname)s: %(message)s'
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+
+INTERNAL_URL = 'internal_url'
+
+
 class InternalSpider(object):
+
     def __init__(self, start_url, max_layer=1):
 
         self._max_layer = max_layer
 
-        parsed = urlparse.urlparse(start_url)
+        parsed = urlparse(start_url)
         scheme = parsed.scheme
         netloc = parsed.netloc
 
@@ -32,32 +44,35 @@ class InternalSpider(object):
 
     def start(self):
         self._session = requests.Session()
+        logging.info('start from %s' % self._url)
         self.get_url(self._url, 1)
-        pass
 
+    @async(15)
     def get_url(self, url, layer):
-
         # 保存
-        print 'save %d layer url:%s' % (layer, url)
+        logging.info('save %d layer url:%s' % (layer - 1, url))
+        succeed = r.sadd(INTERNAL_URL, url)      # 添加到redis的集合中
+
+        if not succeed:
+            logging.info('duplicate %d layer url:%s' % (layer - 1, url))
+            return
 
         if layer > self._max_layer:
             return
 
-        pass
-
+        # 解析网页，提取链接
         session = self._session
-        # session = requests.Session()
         rsp = session.get(url)
 
-        path = urlparse.urlparse(rsp.url).path
-        if path == '/redmine/login':    # 登陆
+        path = urlparse(rsp.url).path
+        if path == '/redmine/login':    # 登陆认证
             payload = {'username': 'ning.xiang', 'password': 'zaq!@#xsw123'}
             rsp = session.post(rsp.url, data=payload)
             url = rsp.url
 
         if not rsp.ok:
             # logging error
-            print 'response error!'
+            logging.info('response error!')
             return
 
         bs = BeautifulSoup(rsp.text, "html.parser")
@@ -68,11 +83,11 @@ class InternalSpider(object):
             if in_url:
                 self.get_url(in_url, layer + 1)
             else:
-                print "filter link:%s" % a['href']
+                logging.info("filter link:%s" % a['href'])
 
     def parse_internal_url(self, parent_url, url):
-        print 'parse internal url, parent=%s, url=%s' % (parent_url, url)
-        parse = urlparse.urlparse(url)
+        logging.info('parse internal url, parent=%s, url=%s' % (parent_url, url))
+        parse = urlparse(url)
         netloc = parse.netloc
         path = parse.path
 
@@ -80,11 +95,9 @@ class InternalSpider(object):
             if path == '':    # 页内跳转
                 return None
             elif path.startswith('/'):  # 根路径
-                return urlparse.urlunparse(parse._replace(scheme=self._scheme, netloc=self._netloc))
-                pass
+                return urlunparse(parse._replace(scheme=self._scheme, netloc=self._netloc))
             else:                       # 相对路径
-                return urlparse.urljoin(parent_url, path)
-                pass
+                return urljoin(parent_url, path)
         elif netloc != self._netloc:
             return None
         else:
@@ -92,37 +105,14 @@ class InternalSpider(object):
 
 
 def main():
-    # proxies = {
-    #     "http": "http://172.26.35.84:808",
-    #     "https": "https://172.26.35.84:808",
-    # }
+    s = time()
+    spider = InternalSpider("http://172.26.35.223/redmine/projects/integration/wiki", 2)
+    spider.start()
+    spider.get_url.join()
+    logging.info(time() - s)
 
-    s = requests.Session()
-    # s.auth = ("ning.xiang", "zaq!@#xsw123")
-
-    r = s.get('http://172.26.35.223/redmine/projects/integration/wiki', allow_redirects=True)
-    # r = s.get('http://172.26.35.223/redmine/login?back_url=http%3A%2F%2F172.26.35.223%2Fredmine%2Fprojects%2Fintegration%2Fwiki', allow_redirects=True)
-    # r = requests.get('http://172.26.35.223/redmine/projects/integration/wiki', allow_redirects=True)
-
-    parsed = urlparse.urlparse(r.url)
-    print r.url, parsed.path
-    print r.history, r.is_redirect
-    print "======================="
-
-    if parsed.path.strip().endswith("login"):
-        payload = {'username': 'ning.xiang', 'password': 'zaq!@#xsw123'}
-        r = s.post(r.url, data=payload)
-
-    parsed = urlparse.urlparse(r.url)
-    print r.url, parsed.path
-    print r.history, r.is_redirect
-
-    print r.status_code, r.cookies
-    print r.headers
-
-    print r.text
+    logging.info('saved %d urls!' % r.scard(INTERNAL_URL))
 
 
 if __name__ == '__main__':
-    # main()
-    InternalSpider("http://172.26.35.223/redmine/projects/integration/wiki").start()
+    main()
